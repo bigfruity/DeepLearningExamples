5bd24abe4b43a45648086f327138fe77bd6f948e
bigfruity <100753139+bigfruity@users.noreply.github.com>
2024-03-15 12:49:52 +1100

    Update model.py
    changed SSD300 to accept num_classes variable

--- PyTorch/Detection/SSD/ssd/model.py
+++ PyTorch/Detection/SSD/ssd/model.py
@@ -53,12 +53,12 @@ class ResNet(nn.Module):
 
 
 class SSD300(nn.Module):
-    def __init__(self, backbone=ResNet('resnet50')):
+    def __init__(self, backbone=ResNet('resnet50'), num_classes=20):
         super().__init__()
 
         self.feature_extractor = backbone
 
-        self.label_num = 81  # number of COCO classes
+        self.label_num = num_classes
         self._build_additional_features(self.feature_extractor.out_channels)
         self.num_defaults = [4, 6, 6, 6, 4, 4]
         self.loc = []
